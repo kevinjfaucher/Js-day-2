6b75348676f7acd28c7548808199c32664a79aa3
Kevin <kfaucher@dtcc.edu>
2024-08-22 17:26:33 -0400

    Added new programs

---
+++ for loops.js
@@ -0,0 +1,23 @@
+for (let count = 1; count < 5; count++) {
+    console.log("Count is: " + count);
+}
+
+
+for (let number = 1; number <= 3; number++) {
+    console.log("Number is: " + number);
+}
+
+
+for (let countdown = 5; countdown > 0; countdown--) {
+    console.log("Countdown: " + countdown);
+}
+
+
+for (let value = 3; value >= 1; value--) {
+    console.log("Value is: " + value);
+}
+
+
+for (let countdown = 10; countdown >= 1; countdown--) {
+    console.log("Countdown: " + countdown);
+}
